f43c644f55b251f1286ce4965ee7dbaef6447eed
Kendall <ktrudickm@gmail.com>
2021-03-17 14:28:33 -0400

    Added files to repository

---
+++ Assets/js/script.js
@@ -0,0 +1,130 @@
+var APIkey = 'c37bbf42'
+var movieSearch = document.getElementById('movieSearch');
+var submitBtn = document.getElementById('submitBtn');
+var movieEl = document.getElementById('movie');
+var playlistEl = document.getElementById('playlist');
+var movieTitle;
+var musicAPIkey;
+var spotifyToken;
+
+a = new URLSearchParams(document.location.search.substr(1))
+a.clientID;
+a.clientSecret;
+
+// Spotify requres a Token to fetch api - this function saves that token.
+function getToken (clientID, clientSecret){
+    const formData = new URLSearchParams();
+    formData.append('grant_type', 'client_credentials');
+
+    fetch('https://accounts.spotify.com/api/token', {
+        headers: {
+            Authorization: `Basic ${btoa(`${clientID}:${clientSecret}`)}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        method: "POST",
+        body: formData.toString()
+    })
+    .then(function (response){
+        return response.json();
+    })
+    .then(function(data){
+        var token = data.access_token;
+        spotifyToken = token;
+        return token;
+    })
+}
+
+// Passes the client ID and client Secret from the URL to the getToken function
+getToken(a.get("clientID"), a.get("clientSecret"))
+
+// Function that uses Spotify API to generate a playlist based on genre from the movie the user searched
+function getPlaylist(Genre) {
+    playlistURL = 'https://api.spotify.com/v1/search?q=' + Genre + '&type=playlist&market=US&limit=5&offset=5'
+    fetch(playlistURL, {
+        headers: {
+            Authorization: `Bearer ${spotifyToken}`
+        }
+    })
+    .then(function(response){
+        return response.json();
+    })
+    .then (function(data){
+        console.log(data);
+        var playlistName = data.playlists.items[1].name;
+        var playlistURL = data.playlists.items[1].external_urls.spotify;
+        var description = data.playlists.items[1].description;
+        var image = data.playlists.items[1].images[0].url;
+        displayPlaylist(playlistName, playlistURL, description, image);
+    })
+}
+
+// Function to capture the movie the user enters and call the getAPI function to use the OMDb API
+function handleSearch(event) {
+    event.preventDefault();
+    movieEl.innerHTML = "";
+    playlistEl.innerHTML = "";
+    movieTitle = movieSearch.value;
+    getAPI(movieTitle)
+}
+
+// Function to display the searched movie onto the page.
+function displayMovie(name, poster, genre) {
+
+    var movieTitle = document.createElement('h2');
+    var movieName = name;
+    movieTitle.textContent = movieName;
+
+    var posterIMG = document.createElement('img');
+    posterIMG.setAttribute('src', poster);
+
+    var genreType = document.createElement('p');
+    genreName = genre;
+    genreType.textContent = "Genre: " + genreName;
+
+    movieEl.append(movieTitle, posterIMG, genreType);
+}
+
+// Function to display Spotify playlist onto the page.
+function displayPlaylist(name, URL, description, image){
+
+    var playlistName = document.createElement('h2');
+    var playName = name;
+    playlistName.textContent = playName;
+
+    var descrip = document.createElement('p');
+    var playDescrip = description;
+    descrip.textContent = playDescrip;
+
+    var display = document.createElement('a')
+    var picture = document.createElement('img');
+    picture.setAttribute('src', image);
+    display.setAttribute('href', URL);
+
+    display.append(picture);
+
+    playlistEl.append(playlistName, descrip, display)
+}
+
+// Function that fetches the OMDb API given the movie the user searches
+function getAPI(movie) {
+    var movieAPI = 'http://www.omdbapi.com/?apikey=' + APIkey + '&t=' + movie;
+    fetch(movieAPI)
+    .then(function(response){
+   
+       if (!response.ok){
+           throw response.json();
+       }
+       return response.json();
+    })
+   
+       .then(function(data){
+           var genre = data.Genre;
+           var genreArr = genre.split(', ');
+           var Genre = genreArr[0];
+           getPlaylist(Genre)
+           displayMovie(data.Title, data.Poster, Genre);
+       })
+};
+
+// Event listener to call the handleSearch function when the search button is pressed
+submitBtn.addEventListener('click', handleSearch);
---
+++ README.md
@@ -0,0 +1,23 @@
+# Bust A Movie
+Your one-stop-shop for finding a playlist that brings you right back to your favorite movies. Search your movie, and we will provide you with a playlist that fits right in with that genre.
+## Usage
+Our group was assigned to create a web application from scratch that serves a purpose in the real world. We have decided to create a movie themed website where the user can search movies and be given feedback on the movies genre and a Spotify playlists associated with the genre. Using a variation of an IMDB API called OMDB and a Spotify API the user would find what genre the movie is such as Action or Comedy and then be able to locate a Spotify playlist with that same genre. Hopefully giving the user songs that they might have heard in a movie they watched and then be able to find it using our web service. The user could also simply use the application to find new music or movies based on shared interests.
+### Contributors
+Reagan Rogers, Kendall Trudick, James Gingrich
+#### Technologies
+Project uses he following languages/frameworks:
+* HTML 5
+* CSS
+* Javascript
+* jQuery
+* Bootstrap 4
+
+##### Url to Deployment and Directions
+https://rrogers422.github.io/MovieDataBase/
+In order for the spotify API to work properply the user needs the have the proper credentials in which we would provide for them in a google document. At the end of the url you would add ?clientID=&clientSecret=
+
+
+###### Screenshot of Applicaton
+<img width="1440" alt="Screen Shot 2021-03-11 at 7 57 06 PM" src="https://user-images.githubusercontent.com/69885419/110875804-4f343100-82a4-11eb-8757-37f1eb677071.png">
+<img width="1440" alt="Screen Shot 2021-03-11 at 7 58 05 PM" src="https://user-images.githubusercontent.com/69885419/110875833-5f4c1080-82a4-11eb-993b-a374503804b5.png">
+<img width="1440" alt="Screen Shot 2021-03-11 at 7 58 17 PM" src="https://user-images.githubusercontent.com/69885419/110875863-6a9f3c00-82a4-11eb-8451-bbcd8384ffa6.png">
